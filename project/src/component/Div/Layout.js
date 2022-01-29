c5475b02c91acf3546e9ad187463b4ea5c10de1f
unknown <kavyasweatha@gmail.com>
2022-01-29 15:29:50 -0600

    footer

--- project/src/component/Div/Layout.js
+++ project/src/component/Div/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Nav from '../home/Nav/Nav';
 import HomeContact from "../home/home_contact/HomeContact";
-
+import Footer from '../home/footer/footer';
 
 export default function Layout(props) {
   return <div >
 <Nav />
 <main>{props.children}</main>
 <HomeContact />
+<Footer />
   </div>;
 }
